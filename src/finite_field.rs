5e0fa0fc371884a8817ed27a776214e3d7ed6d90
Tiago Costa Silva <tiago.costa.silva@gmail.com>
2024-10-19 21:30:55 -0300

    implements Add and Sub traits

--- src/finite_field.rs
+++ src/finite_field.rs
@@ -1,11 +1,13 @@
+use std::ops::{Add, Sub};
+
 #[derive(Debug)]
 pub struct FiniteElement {
-    num: u32,
-    prime: u32,
+    num: i32,
+    prime: i32,
 }
 
 impl FiniteElement {
-    pub fn new(num: u32, prime:u32) -> Self {
+    pub fn new(num: i32, prime: i32) -> Self {
         FiniteElement { num, prime }
     }
 }
@@ -13,6 +15,31 @@ impl FiniteElement {
 impl PartialEq for FiniteElement {
     fn eq(&self, other: &Self) -> bool {
         self.num == other.num && self.prime == other.prime
+    }
+}
+
+impl Add for FiniteElement {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            num: (self.num + other.num) % self.prime,
+            prime: self.prime,
+        }
+    }
+}
+
+impl Sub for FiniteElement {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        // ((a % b) + b) % b // workaround for modulus operation
+        let num = self.num - other.num;
+        let res = ((num % self.prime) + self.prime) % self.prime;
+        Self {
+            num: res,
+            prime: self.prime,
+        }
     }
 }
 
@@ -28,5 +55,29 @@ mod tests {
         assert_eq!(a, b);
         assert_ne!(a, c);
         assert_ne!(b, c);
+    }
+    
+    #[test]
+    fn test_add() {
+        let a = FiniteElement::new(2, 31);
+        let b = FiniteElement::new(15, 31);
+        let res_add_a_b = FiniteElement::new(17, 31);
+        assert_eq!(a + b, res_add_a_b);
+        let c = FiniteElement::new(17, 31);
+        let d = FiniteElement::new(21, 31);
+        let res_add_c_d = FiniteElement::new(7, 31);
+        assert_eq!(c + d, res_add_c_d);
+    }
+    
+    #[test]
+    fn test_sub() {
+        let a = FiniteElement::new(29, 31);
+        let b = FiniteElement::new(4, 31);
+        let res_sub_a_b = FiniteElement::new(25, 31);
+        assert_eq!(a - b, res_sub_a_b);
+        let c = FiniteElement::new(15, 31);
+        let d = FiniteElement::new(30, 31);
+        let res_sub_c_d = FiniteElement::new(16, 31);
+        assert_eq!(c - d, res_sub_c_d);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,9 @@ fn main() {
     let b = FiniteElement::new(6, 13);
     println!("{}", a == b);
     println!("{}", a != b);
+
+    let c = FiniteElement::new(7, 13);
+    let d = FiniteElement::new(12, 13);
+    let e = FiniteElement::new(6, 13);
+    println!("{}", c + d == e);
 }
